b228c1b3c564b780ba8f6712a835d7e0a60cceb8
Roman <299474194+mason82-dotcom@users.noreply.github.com>
2026-09-23 09:12:14 +0200

    feat(android): initialize secure FH2 pairing before MSDK

--- android/fh2-rc-bridge/app/src/main/java/com/fh2/rcbridge/Fh2Application.kt
+++ android/fh2-rc-bridge/app/src/main/java/com/fh2/rcbridge/Fh2Application.kt
@@ -5,6 +5,7 @@ import android.app.Application
 class Fh2Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        Fh2BridgeClient.initialize(this)
         DjiSdkRuntime.start(this)
     }
 }
